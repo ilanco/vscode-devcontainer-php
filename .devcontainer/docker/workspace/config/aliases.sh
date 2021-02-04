11be485986183215f489824866840d82aaebe8b9
Ilan Cohen <ilanco@gmail.com>
2021-02-04 19:40:50 +0000

    add: mongo alias

--- .devcontainer/docker/workspace/config/aliases.sh
+++ .devcontainer/docker/workspace/config/aliases.sh
@@ -1,1 +1,3 @@
 alias composer='docker-compose -f /home/vscode/workspace/vscode-devcontainer-php/.devcontainer/docker-compose.yml -p codespaces-compose run --rm --workdir="$(pwd)" composer'
+
+alias mongo='docker-compose -f /home/vscode/workspace/vscode-devcontainer-php/.devcontainer/docker-compose.yml -p codespaces-compose run --rm --workdir="$(pwd)" mongo mongo'
